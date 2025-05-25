dc50f1fd69a981929e012dbdbf4649c891ba564d
Ron Ochakovsky <ron.ochak@gmail.com>
2025-05-25 10:45:26 +0300

    feat: update candidate creation to handle resume file uploads

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -5,17 +5,18 @@ const pool = require('../config/db');
 const candidateController = {
     // Create a new job candidate
     createCandidate: asyncHandler(async (req, res) => {
-        const { full_name, email, phone, resume_path } = req.body;
+        const { full_name, email, phone } = req.body;
+        const resume_path = req.file ? req.file.path : null;
 
         const query = `
             INSERT INTO public.job_candidates (full_name, email, phone, resume_path)
             VALUES ($1, $2, $3, $4)
             RETURNING *;
         `;
-        const values = [full_name, email, phone, resume_path || "12345.pdf"]
+        const values = [full_name, email, phone, resume_path];
 
         // Await the query
-        const result = await pool.query(query, values)
+        const result = await pool.query(query, values);
         const insertedCandidate = result.rows[0];
 
         console.log('Inserted candidate:', insertedCandidate);
--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const candidateController = require('../controllers/candidateController');
+const upload = require('../config/multer');
 // const validate = require('../middleware/validate');
 // const candidateSchema = require('../validators/candidateValidator');
 
 // POST /api/leads/candidate - Create a new job candidate
-router.post('/', /*validate(candidateSchema),*/ candidateController.createCandidate);
+router.post('/', 
+    upload.single('resume'), // Handle file upload
+    /* validate(candidateSchema), */
+    candidateController.createCandidate
+);
 
 module.exports = router; 

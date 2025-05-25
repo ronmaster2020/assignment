cb1ee2015b8f6de15f57116fcac8dac970679677
Ron Ochakovsky <ron.ochak@gmail.com>
2025-05-26 00:24:07 +0300

    refactor: enhance layout and styling in index.html and App component, remove unused Message component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Image from "./components/Image";
 
 function App() {
   return (
-    <div>
+    <div className="h-full min-h-screen bg-gray-100">
       <Header>
         <Image />
       </Header>

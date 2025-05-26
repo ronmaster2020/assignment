eaf0dcf6de942309ac19ebbeadd76418d5eef100
Ron Ochakovsky <ron.ochak@gmail.com>
2025-05-27 02:02:32 +0300

    refactor: replace useless div with a fragment in HeroSection

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -7,12 +7,12 @@ interface Props {
 
 const HeroSection = ({ title, description }: Props) => {
   return (
-    <div>
+    <>
       <h1 className="main-heading display-4 text-center fw-bold pb-2">
         {title}
       </h1>
       <p className="main-description text-center">{description}</p>
-    </div>
+    </>
   );
 };
 

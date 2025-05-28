1a1eea159e274f850598718203f425ada706b51e
Ron Ochakovsky <ron.ochak@gmail.com>
2025-05-29 00:53:27 +0300

    refactor: make TextSection layout more generic and composable, wrapped icon paragraphs in AppPageTemplate into a TextSection

---
+++ client/src/components/molecules/TextSection.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  heading: string;
+  children: React.ReactNode;
+}
+
+const TextSection = ({ heading, children }: Props) => {
+  return (
+    <>
+      <h5 className="section-heading">{heading}</h5>
+      <div className="section-body d-flex flex-column gap-3 text-start">
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default TextSection;
--- client/src/components/templates/AppPageTemplate.tsx
+++ client/src/components/templates/AppPageTemplate.tsx
@@ -3,7 +3,7 @@ import Header from "../organisms/Header";
 import Footer from "../organisms/Footer";
 import FormContainer from "../organisms/FormContainer";
 import IconParagraph from "../molecules/IconParagraph";
-import TextSection from "../organisms/TextSection";
+import TextSection from "../molecules/TextSection";
 import InfoCard from "../organisms/InfoCard";
 import HeroSection from "../organisms/HeroSection";
 
@@ -44,56 +44,65 @@ const AppPageTemplate = () => {
                   thrive on them."
               />
               {/* Other sections */}
-              <h5 className="section-heading mt-5">Our Services</h5>
-              <IconParagraph
-                icon="/icons/Icon_bolt.png"
-                alt="bolt icon"
-                text="Rapid Project Kickoff: No time to waste. We lump In quickly, deline
-                  critical needs, and set up a dedicated development team that starts
-                  working on your project immediately."
-              />
-              <IconParagraph
-                icon="/icons/Icon_tachometer_alt.png"
-                alt="tachometer icon"
-                text="Accelerated Sprint-Based Development: We work with agile
-                  methodologies, breaking large projects into short, focused sprints,
-                  delivering measurable progress and quality outputs at a fast pace."
-              />
-              <IconParagraph
-                icon="/icons/Icon_cogs.png"
-                alt="cogs icon"
-                text="Expertise in Complex Projects: We have the experience and capability
-                  to handle complex technological projects requiring deep expertise
-                  and creative solutions."
-              />
-              <IconParagraph
-                icon="/icons/Icon_users.png"
-                alt="users icon"
-                text="Elite Dev Teams: We provide access to experienced and professional
-                  development teams, precisely tailored to your project\'s needs and
-                  scope."
-              />
               <div className="mt-5">
-                <TextSection
-                  heading="Our Specialization - Your Advantage!"
-                  paragraph='Sprintignite&apos;s specialty is taking projects that other
-                    companies would call "impossible on this deadline" – and proving otherwise.
-                    We understand that sometimes you need to invest a bit more to get results much faster,
-                    saving precious time that&apos;s worth much more money down the line.
-                    Our approach is "Pay a little more, get it twice as fast" – we don&apos;t compromise on
-                    quality and commit to a speed that will help your startup reach the market before competitors.'
-                />
+                <TextSection heading="Our Services">
+                  <IconParagraph
+                    icon="/icons/Icon_bolt.png"
+                    alt="bolt icon"
+                    text="Rapid Project Kickoff: No time to waste. We lump In quickly, deline
+                          critical needs, and set up a dedicated development team that starts
+                          working on your project immediately."
+                  />
+                  <IconParagraph
+                    icon="/icons/Icon_tachometer_alt.png"
+                    alt="tachometer icon"
+                    text="Accelerated Sprint-Based Development: We work with agile
+                          methodologies, breaking large projects into short, focused sprints,
+                          delivering measurable progress and quality outputs at a fast pace."
+                  />
+                  <IconParagraph
+                    icon="/icons/Icon_cogs.png"
+                    alt="cogs icon"
+                    text="Expertise in Complex Projects: We have the experience and capability
+                          to handle complex technological projects requiring deep expertise
+                          and creative solutions."
+                  />
+                  <IconParagraph
+                    icon="/icons/Icon_users.png"
+                    alt="users icon"
+                    text="Elite Dev Teams: We provide access to experienced and professional
+                          development teams, precisely tailored to your project\'s needs and
+                          scope."
+                  />
+                </TextSection>
+              </div>
+              <div className="mt-5">
+                <TextSection heading="Our Specialization - Your Advantage!">
+                  <p className="section-paragraph text-start pl">
+                    Sprintignite&apos;s specialty is taking projects that other
+                    companies would call "impossible on this deadline" – and
+                    proving otherwise. We understand that sometimes you need to
+                    invest a bit more to get results much faster, saving
+                    precious time that&apos;s worth much more money down the
+                    line. Our approach is "Pay a little more, get it twice as
+                    fast" – we don&apos;t compromise on quality and commit to a
+                    speed that will help your startup reach the market before
+                    competitors.
+                  </p>
+                </TextSection>
               </div>
               <div className="mt-4">
-                <TextSection
-                  heading="Who We Are For?"
-                  paragraph='Startups needing to launch an MVP (Minimum Viable Product) in record
-                time. Companies hitting a development bottleneck and needing a "task
-                force" to break through. Ventures with a short window of opportunity
-                that must release a product quickly. Anyone who understands that time
-                is critical and is willing to invest for a fast, high-quality
-                development solution.'
-                />
+                <TextSection heading="Who We Are For?">
+                  <p className="section-paragraph text-start pl">
+                    Startups needing to launch an MVP (Minimum Viable Product)
+                    in record time. Companies hitting a development bottleneck
+                    and needing a "task force" to break through. Ventures with a
+                    short window of opportunity that must release a product
+                    quickly. Anyone who understands that time is critical and is
+                    willing to invest for a fast, high-quality development
+                    solution.
+                  </p>
+                </TextSection>
               </div>
 
               <h5 className="section-heading mt-5 mb-4">Contact Us</h5>
